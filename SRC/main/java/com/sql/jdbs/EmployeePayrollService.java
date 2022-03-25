b9603ca73cb81327a856f907ffc7e514d1e6936c
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-03-26 01:59:31 +0530

    Added : JDBC

---
+++ SRC/main/java/com/sql/jdbs/EmployeePayrollService.java
@@ -0,0 +1,23 @@
+/**
+ *@PROBLEM_STATEMENT : UC
+ *@author Suraj Chaudhary
+ *@Date 26-Mar-2022
+ */
+package com.sql.jdbs;
+
+import java.util.List;
+
+public class EmployeePayrollService {
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public enum IOSevice {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
+
+	public List<EmployeePayrollData> readEmployeePayrollData(IOSevice ioSevice) {
+		if (ioSevice.equals(IOSevice.DB_IO))
+			this.employeePayrollList = new EmployeePayrollDBService().readData();
+		return employeePayrollList;
+	}
+
+}
